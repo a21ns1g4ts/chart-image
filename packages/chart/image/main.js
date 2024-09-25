2f2da81573931ce61cfa7bf76301588bcdfeecea
a21ns1g4ts <atila.danvi@outlook.com>
2024-09-25 16:10:43 -0300

    Revert api changes chart-image

--- packages/chart/image/main.js
+++ packages/chart/image/main.js
@@ -1,58 +1,21 @@
 const ChartJSImage = require('chart.js-image');
-const atob = require('atob');
-
-function dataURItoBinary(dataURI) {
-    const base64Index = dataURI.indexOf(',') + 1;
-
-    const base64String = dataURI.substring(base64Index);
-
-    const binaryString = atob(base64String);
-
-    return binaryString;
-}
 
 exports.main = async (args) => {
-    const chartData = args
-
-    const returnType = args.returnType || 'binary';
+    const data = args
 
     const barChart = ChartJSImage()
-        .chart(chartData)
+        .chart(data)
         .backgroundColor('white')
         .width(500)
         .height(300);
 
-    let url = null;
-    let dataURI = null;
-    let binary = null;
+        const dataURI = await barChart.toDataURI();
 
-    let responseData = {}
-
-    if(returnType === 'url') {
-        url =  barChart.toURL();
-
-        responseData = {
-            url: url
-        }
-    }
-
-    if(returnType === 'binary') {
-        dataURI = await barChart.toDataURI();
-        binary = dataURItoBinary(dataURI);
-
-        responseData = {
-            binary: binary
-        }
-    }
-
-    if (returnType === 'dataURI' && !dataURI) {
-        dataURI = await barChart.toDataURI();
-
-        responseData = {
+    return {"body": {
+        data: {
+            url: barChart.toURL(),
             dataURI: dataURI
         }
-    }
-
-    return {"body": {data: responseData}};
+    }}
   }
 
