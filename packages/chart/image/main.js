56cbbbdc236a6bbe93d07de9400a4b5e79f13031
a21ns1g4ts <atila.danvi@outlook.com>
2024-09-25 16:00:15 -0300

    Improve return types chart-image

--- packages/chart/image/main.js
+++ packages/chart/image/main.js
@@ -1,21 +1,58 @@
 const ChartJSImage = require('chart.js-image');
+const atob = require('atob');
+
+function dataURItoBinary(dataURI) {
+    const base64Index = dataURI.indexOf(',') + 1;
+
+    const base64String = dataURI.substring(base64Index);
+
+    const binaryString = atob(base64String);
+
+    return binaryString;
+}
 
 exports.main = async (args) => {
-    const data = args
+    const chartData = args
+
+    const returnType = args.returnType || 'binary';
 
     const barChart = ChartJSImage()
-        .chart(data)
+        .chart(chartData)
         .backgroundColor('white')
         .width(500)
         .height(300);
 
-        const dataURI = await barChart.toDataURI();
+    let url = null;
+    let dataURI = null;
+    let binary = null;
 
-    return {"body": {
-        data: {
-            url: barChart.toURL(),
+    let responseData = {}
+
+    if(returnType === 'url') {
+        url =  barChart.toURL();
+
+        responseData = {
+            url: url
+        }
+    }
+
+    if(returnType === 'binary') {
+        dataURI = await barChart.toDataURI();
+        binary = dataURItoBinary(dataURI);
+
+        responseData = {
+            binary: binary
+        }
+    }
+
+    if (returnType === 'dataURI' && !dataURI) {
+        dataURI = await barChart.toDataURI();
+
+        responseData = {
             dataURI: dataURI
         }
-    }}
+    }
+
+    return {"body": responseData}
   }
 
